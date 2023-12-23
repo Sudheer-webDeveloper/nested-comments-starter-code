d1c8e73e838cb5198658dcd7daf37cd913266307
Sudheer-webDeveloper <sudheertalaudi@gmail.com>
2023-12-23 22:59:14 +0530

    first commit

---
+++ src/components/comments/comments.js
@@ -0,0 +1,16 @@
+const Comments = () => {
+  return (
+    <div>
+      <div className="comment-container">
+        <h3>Hello! How are you?</h3>
+        <div>
+          <button>Reply</button>
+          <button>Delete</button>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Comments;
